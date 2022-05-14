0c77f20aa4ea03296d5a36a1dc7a6af6ee4491ce
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-05-15 00:19:01 +0200

    init routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Create from "./pages/Create";
+import Project from "./pages/Project";
 
 function App() {
-  return <div>app</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="create" element={<Create />} />
+        <Route path="projects/:id" element={<Project />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
